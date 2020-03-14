ca117ba8c782aad641caad36d7c909c33e122a0a
toba101 <toba.obiwale@outlook.com>
2020-03-14 18:32:29 +0000

    recent-edit

---
+++ assignments/lesson-10/script/functions.js
@@ -0,0 +1,52 @@
+// Current Date //
+var d = new Date();
+var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+var days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+
+document.getElementById("currentDate").innerHTML = days[d.getDay()] + ' '+ months[d.getMonth()] +' '+ d.getDate() +' '+ d.getFullYear();
+
+// Hamburger //
+function toggleNavMenu() {
+    document.getElementById("myNav").classList.toggle("hide");
+}
+
+// Severity //
+function adjustRating(rating) {
+    document.getElementById("ratingvalue").innerHTML = rating;
+}
+
+// Weather API //
+let weatherRequest = new XMLHttpRequest();
+var apiURLstring = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=b2e353ce185f86c20ab99b14bda3efa7';
+weatherRequest.open('GET', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload =  function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+document.getElementById("currently").innerHTML = weatherData.weather[0].description;
+document.getElementById("currentTemp").innerHTML = weatherData.main.temp;
+document.getElementById("Humidity").innerHTML = weatherData.main.humidity;
+document.getElementById("windSpeed").innerHTML = weatherData.wind.speed;
+
+ var temp = weatherData.main.temp;
+ var speed = weatherData.wind.speed;
+
+ var result = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed,0.16) + 0.4275 *
+  temp * Math.pow(speed, 0.16);
+
+  document.getElementById('windChill').innerHTML = result.toFixed(1);
+}
+
+// Webfront Load //
+webFont.load({google:{familie: ["Lato","san-serif"]}});
+
+
+// Windchill //
+var speed = parseInt(document.getElementById('windSpeed').innerHTML);
+var temp = parseInt(document.getElementById('currentTemp').innerHTML);
+
+var windChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
+windChill = Math.round(windChill);
+document.getElementById("windChill").innerHTML = windChill;
